ea1c6586ad7b6d027c4f2cd2a40568a0592615e3
Chane Eshetu <49794721+chandem@users.noreply.github.com>
2023-04-17 13:45:59 +0300

    Update 2-arrow.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,9 +2,9 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   this.addNeighborhood = (newNeighborhood) => { 
-    // eslit disable next-line
+    
    this.sanFranciscoNeighborhoods.push(newNeighborhood);  
-   //eslit disable next-line 
+   
    return this.sanFranciscoNeighborhoods;
   };
 
