73f55afe57ce6e3f519a21ad88e99b517e0d15cb
Chane Eshetu <49794721+chandem@users.noreply.github.com>
2023-04-17 07:56:21 +0300

    Create 2-arrow.js

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,8 @@
+export default getNeighborhoodsList = () => {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
