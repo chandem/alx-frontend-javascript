cff93b358f6fbcd94a7f389125d05a9dbe5ee5b3
Chane Eshetu <49794721+chandem@users.noreply.github.com>
2023-04-17 23:19:17 +0300

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,18 +1,12 @@
-export default function createIteratorObject(report) { 
- let departments = Object.values(report); 
- let employees = []; 
- for (let i = 0; i < departments.length; i++) {  
-  employees.push(...Object.values(departments[i])); 
- }  
-let index = 0;  
-return { 
-next: function() {   
-   if (index < employees.length) {     
-   return { value: employees[index++], done: false };   
-   }
- else {   
-     return { done: true };   
-   }  
-  };  
+export default function createIteratorObject(report) {
+  const employees = [];
+  
+	for (const dep of Object.keys(report.allEmployees)) {
+    
+		for (const employee of report.allEmployees[dep]) {
+      employees.push(employee);
+    }
+  }
+
+  return employees;
 }
- 
