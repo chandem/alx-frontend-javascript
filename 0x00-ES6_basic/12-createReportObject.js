5bc71027d6bc566dbce355987a594655b3e9c522
Chane Eshetu <49794721+chandem@users.noreply.github.com>
2023-04-17 22:16:42 +0300

    Update 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,22 +1,12 @@
 import createEmployeesObject from './11-createEmployeesObject.js';
 
 export default function createReportObject(employeesList) {
-  const allEmployees = {};
-
-  for (const employee of employeesList) {
-    const { department, name } = employee;
-
-    if (!allEmployees.hasOwnProperty(department)) {
-      allEmployees[department] = [name];
-    } else {
-      allEmployees[department].push(name);
-    }
-  }
-
   return {
-    allEmployees,
-    getNumberOfDepartments: function() {
-      return Object.keys(allEmployees).length;
-    }
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(empList) {
+      return Object.keys(empList).length;
+    },
   };
 }
