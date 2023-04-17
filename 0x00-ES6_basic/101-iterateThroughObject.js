e6c75a8b60206199f71ac546bb9a8077a05f583b
Chane Eshetu <49794721+chandem@users.noreply.github.com>
2023-04-17 23:25:23 +0300

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,11 +1,4 @@
-export default function iterateThroughObject(reportWithIterator) { 
- let employeeNames = '';  
-const allEmployees = reportWithIterator.next().value.allEmployees; 
- for (const department in allEmployees) { 
-   for (const employee of allEmployees[department]) {   
-   employeeNames +=  ${employee} |  ;  
-  } 
- } 
- return employeeNames.slice(0, -3);
-} 
+export default function iterateThroughObject(reportWithIterator) {
+  return reportWithIterator.join(' | ');
+}
 
