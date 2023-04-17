a4be1884a0f67c69fad201d4ee7aec702a582da6
Chane Eshetu <49794721+chandem@users.noreply.github.com>
2023-04-17 07:31:34 +0300

    Create 0-constants.js

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,17 @@
+const taskFirst = () => {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+let taskNext = () => {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
+
+export { taskFirst, taskNext };
