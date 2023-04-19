7f9706cd51303021b13451fdba942f6a344aefea
Chane Eshetu <49794721+chandem@users.noreply.github.com>
2023-04-19 07:50:40 +0300

    Update 9-getFullBudget.js

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,3 +1,4 @@
+import getBudgetObject from './7-getBudgetObject.js';
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
@@ -11,3 +12,4 @@ export default function getFullBudgetObject(income, gdp, capita) {
   };
   return fullBudget;
 }
+
