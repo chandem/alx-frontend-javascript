ed692f514f10e877eab49026174f83b027c3da7a
Chane Eshetu <49794721+chandem@users.noreply.github.com>
2023-04-20 12:22:38 +0300

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,7 @@
-const ClassRoom = require('./0-classroom');
-
+import ClassRoom from "./0-classroom.js";
 function initializeRooms() {
   const room1 = new ClassRoom(19);
   const room2 = new ClassRoom(20);
   const room3 = new ClassRoom(34);
   return [room1, room2, room3];
 }
-
-module.exports = initializeRooms;
