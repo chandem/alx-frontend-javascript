d8c4a3224e79e6e929992f910ea56be809e7da01
Chane Eshetu <49794721+chandem@users.noreply.github.com>
2023-04-20 14:06:51 +0300

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,15 +2,15 @@ import Building from "./5-building.js";
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-   if(evacuationWarningMessage()) {
-    return `Evacuate slowly the ${this._floors} floors.`;
-  }
     super(sqft);
     this._floors = floors;
   }
 
   get floors() {
     return this._floors;
+  }
+  evacuationWarningMessage(){
+    return `Evacuate slowly the ${this._floors} floors.`;
   }
 
   
